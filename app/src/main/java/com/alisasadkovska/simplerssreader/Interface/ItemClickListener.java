6445e8b2c99b9f8cf42602fb0504f2720c4f5030
alisasadkovska <54442838+alisasadkovska@users.noreply.github.com>
2020-09-12 16:36:31 +0300

    1.1

--- app/src/main/java/com/alisasadkovska/simplerssreader/Interface/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.alisasadkovska.simplerssreader.Interface;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onClick(View view, int position, boolean isLongClick);
-}
--- app/src/main/java/com/alisasadkovska/simplerssreader/Interface/RecyclerItemTouchHelperListener.java
+++
@@ -1,7 +0,0 @@
-package com.alisasadkovska.simplerssreader.Interface;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public interface RecyclerItemTouchHelperListener {
-    void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position);
-}
